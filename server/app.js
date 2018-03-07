99f8b266b1c315624d8f27c174dbde2895e5da10
Loïc Mauvoisin <loic.mauvoisin@edu.itescia.fr>
2018-03-07 15:27:17 +0100

    modifToken

--- server/app.js
+++ server/app.js
@@ -8,10 +8,10 @@ const io = require('socket.io')(server);
 const Twit = require('twit');
 
 let twit = new Twit({
-    consumer_key: 'USjc3Ro0NmTqfIfVQivQHbfCN',
-    consumer_secret: 'PepvRacuns9tccjfzCT4daWjdb14NbP9wbnfEJZfrVk4uN0rUk',
-    access_token: '96856540-esqyWosyLoTmpwZygxGH5eDfN8Fn5Q39rljZ4qqqn',
-    access_token_secret: 'GAtZsH9C39Wq6OVgGRtkCPGE26ovpvu0yZml8EmoD8LGE'
+    consumer_key: 'QwHnUXfbGkEAn0Q0vy5GTtCP7',
+    consumer_secret: 'WZmWMCGWVEiWFEpdUm5U4UYgSh7cF8vPL2kVzY0aukAFeHef5N',
+    access_token: '359847562-Z3Q56EzbMu6GXwGkRefDkI0A9IzavzdKHy8KjGtC',
+    access_token_secret: 'SrTuuj2N6SZNET7kbldRRi5lTOqv617biGZGWVuAjOSc3'
 });
 
 let allTweets = [];
